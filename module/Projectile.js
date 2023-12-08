59752be5ecb1dfc9aa861c6e92e7d7c0aad20378
Mogrima <rebel.rs@yandex.ru>
2023-12-08 17:11:45 +0500

    добавлено изображение для снарядов и анимировано

--- module/Projectile.js
+++ module/Projectile.js
@@ -4,10 +4,14 @@ export class Projectile {
         this.x = x;
         this.y = y;
         this.direct = direct;
-        this.width = 20;
-        this.height = 13;
+        this.width = 16;
+        this.height = 16;
         this.speed = 8;
         this.markedForDeletion = false;
+        this.image = document.getElementById('projectile3');
+        this.frameX = 0;
+        this.frameY = 0;
+        this.maxFrame = 14;
     }
   
     update() {
@@ -16,14 +20,21 @@ export class Projectile {
             if (this.x > this.game.width * 0.9) this.markedForDeletion = true;
            
         } else if (this.direct === "left") {
+            this.frameY = 1;
             this.x -= this.speed;
             if (this.x < this.game.width * 0.1) this.markedForDeletion = true;
+        }
+        if(this.frameX < this.maxFrame) {
+            this.frameX++;
+        } else {
+            this.frameX = 0;
         }
         
     }
   
     draw(context) {
-        context.fillStyle = '#120a8f';
-        context.fillRect(this.x, this.y, this.width, this.height);
+        // context.fillStyle = '#120a8f';
+        // context.fillRect(this.x, this.y, this.width, this.height);
+        context.drawImage(this.image, 16 * this.frameX, this.height * this.frameY, 16, 16, this.x, this.y,  26, 26);
     }
 }
