cef3c502140f751978d25b7c8ffbd3d9979bec4b
Mogrima <rebel.rs@yandex.ru>
2023-12-14 20:19:08 +0500

    уменьшин первый враг в размерах

--- module/Enemies/monster1.js
+++ module/Enemies/monster1.js
@@ -3,16 +3,16 @@ import {Enemy} from './Enemy.js';
 export class Monster1 extends Enemy {
     constructor(game) {
         super(game);
-        this.width = 131;
-        this.height = 131;
+        this.width = 32;
+        this.height = 28;
 
         this.sWidth = 32;
         this.sWheight = 28;
 
-        this.dWidth = 131;
-        this.dHeight = 131;
+        this.dWidth = 32 *2;
+        this.dHeight = 28 *2;
 
-        this.y = game.height - this.height - 25;
+        this.y = game.height - this.height - 75;
         this.image = document.getElementById('enemy3');
         this.frameY = 0;
         this.maxFrame = 7;
