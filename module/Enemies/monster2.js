415c100a29029213a03cce31fd6ceccf565abf49
Mogrima <rebel.rs@yandex.ru>
2023-12-20 19:35:15 +0500

    подключен новый второй враг

--- module/Enemies/monster2.js
+++ module/Enemies/monster2.js
@@ -3,19 +3,19 @@ import {Enemy} from './Enemy.js';
 export class Monster2 extends Enemy {
     constructor(game) {
         super(game);
-        this.width = 48;
-        this.height = 48;
+        this.width = 80;
+        this.height = 70;
 
-        this.sWidth = 48;
-        this.sWheight = 48;
+        this.sWidth = 80;
+        this.sWheight = 70;
 
-        this.dWidth = 96;
-        this.dHeight = 96;
+        this.dWidth = 80 * 1.5;
+        this.dHeight = 70 * 1.5;
 
         this.y = game.height - this.height - 90;
         this.image = document.getElementById('enemy2');
-        this.frameY = 1;
-        this.maxFrame = 7; 
+        this.frameY = 4;
+        this.maxFrame = 3; 
         this.lives = 5;
         this.score = this.lives;
 
@@ -24,7 +24,7 @@ export class Monster2 extends Enemy {
             this.x = this.game.width;
         } else {
             this.x = 0;
-            this.frameY = 0;
+            this.frameY = 5;
         }
     }
 }
