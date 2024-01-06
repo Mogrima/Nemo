ffa037683cdfa33d7a37a8a60efe9c1d1e54152a
Mogrima <rebel.rs@yandex.ru>
2024-01-06 19:12:01 +0500

    переименованы свойства сравниваемых объектов в методе checkCollision

--- module/Game.js
+++ module/Game.js
@@ -195,10 +195,10 @@ export class Game {
 
     checkCollision(rect1, rect2) {
         return (
-            rect1.x < rect2.x + rect2.width &&
-            rect2.x < rect1.x + rect1.width &&
-            rect1.y < rect2.y + rect2.height &&
-            rect2.y < rect1.y + rect1.height);
+            rect1.collisionX < rect2.collisionX + rect2.width &&
+            rect2.collisionX < rect1.collisionX + rect1.width &&
+            rect1.collisionY < rect2.collisionY + rect2.height &&
+            rect2.collisionY < rect1.collisionY + rect1.height);
     }
 
     isWin() {
