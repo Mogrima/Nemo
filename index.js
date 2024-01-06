7eda68334bed1e0ebdf8dfdb095f0121ffe02012
Mogrima <rebel.rs@yandex.ru>
2024-01-06 20:32:34 +0500

    исправлены ошибки eslint

--- index.js
+++ index.js
@@ -1,12 +1,12 @@
-"use strict";
-import {Game} from './module/Game.js';
+'use strict';
+import { Game } from './module/Game.js';
 
 window.addEventListener('load', function () {
     // canvas setup
     const canvas = this.document.getElementById('canvas');
     const ctx = canvas.getContext('2d');
     canvas.width = 1500;
-    canvas.height = 600; 
+    canvas.height = 600;
 
     const game = new Game(canvas.width, canvas.height);
     game.addProps();
