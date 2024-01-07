00fc0cfdd4d539c932d38cdc81455edbee4050ab
Mogrima <rebel.rs@yandex.ru>
2024-01-07 06:57:03 +0500

    id изображения для каждого объекта дерева генерируется рандомно, чтобы реализовать разнообразие отрисованных деревьев

--- UI/Tree.js
+++ UI/Tree.js
@@ -3,14 +3,18 @@ export class Tree {
         this.canvas = canvas;
         this.canvasWidth = this.canvas.width;
         this.canvasHeight = this.canvas.height * 0.5;
-        this.spriteWidth = 200;
-        this.spriteHeight = 200;
-        this.width = this.spriteWidth;
-        this.height = this.spriteHeight;
+        // this.spriteWidth = 200;
+        // this.spriteHeight = 200;
+        // this.width = this.spriteWidth;
+        // this.height = this.spriteHeight;
         this.x = Math.random() * this.canvasWidth;
         this.y = (Math.random() * (this.canvasHeight * 0.5 - 50) + 50);
         this.spriteX = this.x;
         this.spriteY = this.y;
+        
+        this.id = Math.floor(Math.random() * (5 + 1) + 2);
+        
+        this.image = document.getElementById('tree' + this.id);
     }
 
     update() {
