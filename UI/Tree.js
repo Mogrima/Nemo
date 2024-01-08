ca6be1a5dd923cf3cbb4c6772aa23a48307d1cc3
Mogrima <rebel.rs@yandex.ru>
2024-01-08 07:17:54 +0500

    отрегулированы вертикальные границы отрисовки деревьев

--- UI/Tree.js
+++ UI/Tree.js
@@ -8,13 +8,17 @@ export class Tree {
         // this.width = this.spriteWidth;
         // this.height = this.spriteHeight;
         this.x = Math.random() * this.canvasWidth;
-        this.y = (Math.random() * (this.canvasHeight * 0.5 - 50) + 50);
-        this.spriteX = this.x;
-        this.spriteY = this.y;
+        // this.y = (Math.random() * (this.canvasHeight * 0.5 - 50) + 50);
+
         
         this.id = Math.floor(Math.random() * (5 + 1) + 2);
         
         this.image = document.getElementById('tree' + this.id);
+        this.y = (Math.random() * (350 - 250) + 250) - this.image.height;
+        this.spriteX = this.x;
+        this.spriteY = this.y;
+        
+        console.log(this.canvasHeight)
     }
 
     update() {
