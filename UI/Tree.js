d2f030e3f40ab850057aef6f0dbf444ce6437005
Mogrima <rebel.rs@yandex.ru>
2024-01-11 06:40:36 +0500

    добавлены св-ва ширины и высоты, которые получают значения от spriteWidth и spriteHeight соответственно - для будущей масштабируемости изображений деревьев. this.image.height и this.image.width вынесены в отдельные св-ва для большей гибкости их использования за пределами класса

--- UI/Tree.js
+++ UI/Tree.js
@@ -2,17 +2,13 @@ export class Tree {
     constructor(fieldWidth, fieldHeight) {
         this.fieldWidth = fieldWidth;
         this.fieldHeight = fieldHeight * 0.5;
-        
-        // this.spriteWidth = 200;
-        // this.spriteHeight = 200;
-        // this.width = this.spriteWidth;
-        // this.height = this.spriteHeight;
-        this.x = Math.random() * this.fieldWidth;
-
-        
         this.id = Math.floor(Math.random() * (5 + 1) + 2);
-        
         this.image = document.getElementById('tree' + this.id);
+        this.spriteWidth = this.image.width;
+        this.spriteHeight = this.image.height;
+        this.width = this.spriteWidth;
+        this.height = this.spriteHeight;
+        this.x = Math.random() * this.fieldWidth;
         this.y = (Math.random() * (350 - 250) + 250) - this.image.height;
         this.spriteX = this.x;
         this.spriteY = this.y;
