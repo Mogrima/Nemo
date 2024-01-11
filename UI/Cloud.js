2d4f20bf731882cea79f358ae94c5f91853d93e6
Mogrima <rebel.rs@yandex.ru>
2024-01-11 08:15:02 +0500

    добавлены св-ва ширины и высоты, которые получают значения от spriteWidth и spriteHeight соответственно - для будущей масштабируемости изображений деревьев.

--- UI/Cloud.js
+++ UI/Cloud.js
@@ -2,8 +2,10 @@ export class Cloud {
     constructor(fieldWidth, fieldHeight) {
         this.fieldWidth = fieldWidth;
         this.fieldHeight = fieldHeight * 0.5;
-        this.width = 100;
-        this.height = 53;
+        this.spriteWidth = 100;
+        this.spriteHeight = 53;
+        this.width = this.spriteWidth;
+        this.height = this.spriteHeight;
         this.speedX = Math.random() * + 0.5;
         this.cloudImage = document.getElementById('cloud');
         this.x = Math.random() * this.fieldWidth;
