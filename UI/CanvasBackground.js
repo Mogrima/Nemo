6cdbe2bf181fa240eb98dc3d4ed771c96a8af9b2
Mogrima <rebel.rs@yandex.ru>
2024-01-11 08:25:18 +0500

    удалены неиспользуемые свойства класса и закомментированный код

--- UI/CanvasBackground.js
+++ UI/CanvasBackground.js
@@ -7,17 +7,8 @@ export class CanvasBackground {
         this.canvasWidth = this.canvas.width;
         this.canvasHeight = this.canvas.height;
 
-        // sky
-        // this.skyHeight = this.canvasHeight * 0.5;
-        // this.skyColor = "#00416a";
         this.sky = new Sky(canvas);
-
-        // forest
-        // this.forestHeight = this.canvasHeight * 0.3;
-        // this.forestColor = "#228b22";
-        this.forestYstart = this.canvasHeight * 0.2 + 100;
         this.forest = new Forest(canvas);
-        
 
         // game field
         this.gameFieldHeight = this.canvasHeight * 0.5;
@@ -35,19 +26,12 @@ export class CanvasBackground {
     
     draw(context) {
         context.save();
-        // sky
-        // context.fillStyle = this.skyColor;
-        // context.fillRect(0, 0, this.canvasWidth, this.skyHeight);
         this.sky.draw(context);
         context.drawImage(this.imageCity, this.canvas.height, 175);
 
-        // forest
-        // context.fillStyle = this.forestColor;
-        // context.fillRect(0, this.forestYstart, this.canvasWidth, this.forestHeight);
-        
-
         // game field
         context.fillStyle = this.gameFieldColor;
+
         context.fillRect(0, this.gameFieldYstart, this.canvasWidth, this.gameFieldHeight);
         context.drawImage(this.imagePlansBackground, 0, this.gameFieldYstart - 180);
         this.forest.draw(context);
