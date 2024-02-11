c633b34c9a974f7ef4f0f868004233a0e797283f
Mogrima <rebel.rs@yandex.ru>
2024-02-11 21:42:43 +0500

    добавлены пустые массивы снарядов для их сброса при перезапуске игры

--- module/Units/Nemo.js
+++ module/Units/Nemo.js
@@ -55,6 +55,8 @@ export class Nemo extends Unit {
         this.frameX = 0;
         this.frameY = 1;
         this.numberOfCorpuscle = 20;
+        this.ammunition = [];
+        this.splashes = [];
     }
 
 }
