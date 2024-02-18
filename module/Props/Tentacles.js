cbaca52d03f899fcec08d19210c1e07cad24f81b
Mogrima <rebel.rs@yandex.ru>
2024-02-18 23:43:11 +0500

    Проигрыш анимации исчезновения щупалец возвращен в блоки кода в методе update.

--- module/Props/Tentacles.js
+++ module/Props/Tentacles.js
@@ -6,7 +6,7 @@ export class Tentacles {
         this.image = document.getElementById('tentacles');
         this.collisionX = Math.random() * this.game.width;
         this.collisionY = (Math.random() * (this.game.topMargin - this.game.height * 0.82) +
-                            this.game.height * 0.82);
+            this.game.height * 0.82);
         this.spriteWidth = 50;
         this.spriteHeight = 87;
         this.width = 25;
@@ -18,16 +18,16 @@ export class Tentacles {
         this.spriteY = this.collisionY;
         this.fps = 20;
         this.timer = 0;
-        this.interval = 1000/this.fps;
+        this.interval = 1000 / this.fps;
         this.lives = 3;
 
         this.getFeature = this.game.prop.getFeature();
         this.feature = this.getFeature[0];
-        this.featureName =  this.getFeature[1];
+        this.featureName = this.getFeature[1];
 
         this.text = this.getFeature[2] || null;
     }
-    
+
     update(deltaTime, context) {
         this.handleFrames(deltaTime);
 
@@ -40,30 +40,51 @@ export class Tentacles {
             }
         });
 
-           if (this.propTrigger()) {
+        if (this.propTrigger()) {
             if (this.featureName === 'strangeMessage') {
                 if (!this.game.toggleMessage) {
-                    this.disappearingTentacles(deltaTime);
-                    this.game.sound.strangeMessageSound.play();
-                    this.feature(context); 
-                } else {
-                        this.remove();
-                        for (let i = 0; i < 5; i++) {
-                            this.game.corpuscles.add(new Spark(this.game, this.collisionX,
-                                this.collisionY + this.height, 'gold'));
+                    if (this.frameX > 0) {
+                        this.game.sound.tentacles();
+                        if (this.timer > this.interval) {
+                            this.frameX--;
+                            this.timer = 0;
+                        } else {
+                            this.timer += deltaTime;
                         }
-                } 
-            } 
-            else {
-                this.disappearingTentacles(deltaTime);
-                this.feature(context);
-                this.remove();
-                for (let i = 0; i < 5; i++) {
-                    this.game.corpuscles.add(new Spark(this.game, this.collisionX,
-                        this.collisionY + this.height, '#0000ff'));
+
+                    } else {
+                        this.game.sound.strangeMessageSound.play();
+                        this.feature(context);
+                    }
+
+                } else {
+                    this.remove();
+                    for (let i = 0; i < 5; i++) {
+                        this.game.corpuscles.add(new Spark(this.game, this.collisionX,
+                            this.collisionY + this.height, 'gold'));
+                    }
                 }
+            } else {
+                if (this.frameX > 0) {
+                    this.game.sound.tentacles();
+                    if (this.timer > this.interval) {
+                        this.frameX--;
+                        this.timer = 0;
+                    } else {
+                        this.timer += deltaTime;
+                    }
+
+                } else {
+                    this.feature(context);
+                    this.remove();
+                    for (let i = 0; i < 5; i++) {
+                        this.game.corpuscles.add(new Spark(this.game, this.collisionX,
+                            this.collisionY + this.height, '#0000ff'));
+                    }
+                }
+
             }
-    }
+        }
 
     }
 
@@ -79,29 +100,14 @@ export class Tentacles {
         context.drawImage(this.image,
             this.frameX * this.spriteWidth, this.frameY * this.spriteHeight,
             this.spriteWidth, this.spriteHeight,
-            this.spriteX, this.spriteY, this.spriteWidth, this.spriteHeight,);
+            this.spriteX, this.spriteY, this.spriteWidth, this.spriteHeight, );
     }
 
     propTrigger() {
         return ((this.feature !== null) &&
-                (!this.gameOver) &&
-                (this.lives < 1)) 
+            (!this.gameOver) &&
+            (this.lives < 1))
 
-    }
-
-    disappearingTentacles(deltaTime) {
-        if (this.frameX > 0) {
-            this.game.sound.tentacles();
-            if (this.timer > this.interval) {
-                this.frameX--;
-                this.timer = 0;
-            } else {
-                this.timer += deltaTime;
-            }
-            
-        } else {
-            return;
-        }
     }
 
     handleFrames(deltaTime) {
